468b314e46dd3595ec1ae2d52bc0c1e0f885863e
jaenwawe <nwawe.jae@gmail.com>
2021-12-03 06:39:29 -0600

    updated the props

--- client/src/components/Me.js
+++ client/src/components/Me.js
@@ -3,21 +3,19 @@ import {made_image} from "../data/Mock"
 
 
 function Me ({currentUser}) {
+
+
     return (
-<div> 
-  <h1>currentUser.firstName</h1>
-              <h2>First class clothes  Resold at great prices</h2>
-              <img src={made_image} alt="girl looking up" />
-              <p>Shop online!  There is no need to leave your house to find vintage clothing, or even a toaster</p>
-              <p>The sellers can relate to seller online and not having to get to a vintage store since most do not have contactless delivery. If you see any, tell everyone</p>
-
-              <p>View the products that are listed on each page other than the signup and sale page. </p>
-              <p> On the product that you like click the add to cart button to create a list of what you intend to buy once you have logged in or registered. </p>
-              <p>Login by clicking the Login button on the top of the page or sign up to be a member by clicking the Sign up button on the navigation bar at the top of the page</p>
-              <p>Once you have logged in or Registered, you can post products to the list of products by Clicking the Sale button on the navigation bar at the top of the page </p>
-              <p>The items that you have added are in the Checkout button on the navigation bar at the top of the page</p>
-              <p>To Checkout select the method of payment and click submit.  You will see the total cost of the cart and the items below</p>
-
+<div>
+  <h1> Profile</h1> 
+  <h5>{ currentUser.first_name}  {currentUser.last_name}</h5>
+  <h5>{ currentUser.username} </h5>
+  <h5>{ currentUser.email} </h5> 
+  <h5>{currentUser.phone_number}</h5>
+  <h5>{ currentUser.address} </h5>
+  <h5>{ currentUser.city}</h5> 
+  <h5>{currentUser.state}</h5>
+  <h5>{ currentUser.zipcode} </h5>
 
   </div>    
     );
